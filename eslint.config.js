b9f04bdfcabb7858deb6e037137bc87bf41b927f
Juraj Žec <juro.zec.1@gmail.com>
2023-12-02 15:59:26 +0100

    Initial project setup - TypeScript, ESLint, Prettier, Express

---
+++ eslint.config.js
@@ -0,0 +1,68 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import typescriptParser from '@typescript-eslint/parser'
+import tsEslintPlugin from '@typescript-eslint/eslint-plugin'
+import eslintPrettierPlugin from 'eslint-plugin-prettier'
+import eslintUnusedImportsPlugin from 'eslint-plugin-unused-imports'
+import importPlugin from 'eslint-plugin-import'
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    ignores: ['global.d.ts'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+      globals: globals.node,
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    plugins: {
+      '@typescript-eslint': tsEslintPlugin,
+      prettier: eslintPrettierPlugin,
+      'unused-imports': eslintUnusedImportsPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      // general
+      'no-trailing-spaces': 'error',
+      'quote-props': ['error', 'as-needed'],
+      quotes: ['error', 'single'],
+      'sort-imports': ['error', { ignoreDeclarationSort: true }],
+      'object-property-newline': [
+        'error',
+        { allowAllPropertiesOnSameLine: false },
+      ],
+      'comma-dangle': ['error', 'always-multiline'],
+      semi: ['error', 'never'],
+      'eol-last': ['error', 'always'],
+      'arrow-body-style': ['error', 'as-needed'],
+      'no-multi-spaces': 'error',
+      'object-shorthand': 'error',
+
+      // @typescript-eslint/recommended-type-checked rule set
+      ...tsEslintPlugin.configs['recommended-type-checked'].rules,
+
+      // prettier
+      'prettier/prettier': 'error',
+
+      // unused-imports
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': 'warn',
+
+      // consistent-type-imports
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+    },
+  },
+]
---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Linter} */
+const config = {
+  bracketSpacing: true,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+}
+
+module.exports = config
